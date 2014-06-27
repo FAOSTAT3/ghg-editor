8eabe0688b0f24134095c24f8f4ba90c8e50e1d2
guidofenix <guido.barbaglia@fao.org>
2014-06-27 15:41:48 +0200

    Dynamic charts

--- js/ghg-editor.js
+++ js/ghg-editor.js
@@ -5,42 +5,104 @@ var GHGEDITOR = (function() {
     };
 
     function init() {
+
         $('.selector').chosen({
             disable_search_threshold: 10,
             allow_single_deselect: true
         });
-        $('input[type="number"]').keyup(function() {
+
+        $('input').keyup(function() {
             var v = parseFloat($('#' + this.id).val());
             if (isNaN(v) || v < 0) {
-                console.log('unvalid selection @ ' + this.id);
+                console.log('invalid selection @ ' + this.id);
             } else {
                 var year = this.id.substring(1 + this.id.indexOf('_'));
                 addPointToChart1(year, v);
             }
         });
-        queryDB();
+
+        var series_1 = [
+            {
+                name: 'Agricultural Total (FAOSTAT)',
+                domain: 'GT',
+                country: '48',
+                item: '1711',
+                element: '7231'
+            }
+        ];
+        createChart('chart_1', 'Agricultural Total', series_1);
+
+        var series_2 = [
+            {
+                name: 'Enteric Fermentation (FAOSTAT)',
+                domain: 'GT',
+                country: '48',
+                item: '5058',
+                element: '7231'
+            },
+            {
+                name: 'Manure Management (FAOSTAT)',
+                domain: 'GT',
+                country: '48',
+                item: '5059',
+                element: '7231'
+            }
+        ];
+        createChart('chart_2', 'Enteric Fermentation and Manure Management', series_2);
+
+        var series_3 = [
+            {
+                name: 'Rice Cultivation (FAOSTAT)',
+                domain: 'GT',
+                country: '48',
+                item: '5060',
+                element: '7231'
+            },
+            {
+                name: 'Agricultural Soils (FAOSTAT)',
+                domain: 'GT',
+                country: '48',
+                item: '1709',
+                element: '7231'
+            }
+        ];
+        createChart('chart_3', 'Rice Cultivation and Agricultural Soils', series_3);
+
+        var series_4 = [
+            {
+                name: 'Burning of Savanna (FAOSTAT)',
+                domain: 'GT',
+                country: '48',
+                item: '5067',
+                element: '7231'
+            },
+            {
+                name: 'Burning of Crop Residues (FAOSTAT)',
+                domain: 'GT',
+                country: '48',
+                item: '5066',
+                element: '7231'
+            }
+        ];
+        createChart('chart_4', 'Burning of Savanna and Burning of Crop Residues', series_4);
+
     };
 
-    function createCharts() {
-        var data = [];
-        var year = 1990;
-        for (var i = 1 ; i < GHGEDITOR.CONFIG.data.length ; i++) {
-            var tmp = [];
-            tmp.push(parseInt(year))
-            tmp.push(parseFloat(GHGEDITOR.CONFIG.data[i][13]));
-            data.push(tmp);
-            year++;
-        }
-        createChart1(data);
-    };
-
-    function createChart1(data) {
-        var categories = [];
-        for (var i = 1990 ; i <= 2015 ; i++)
-            categories.push(i);
-        GHGEDITOR.CONFIG.chart_1 = $('#chart_1').highcharts({
+    function createChart(chart_id, title, series) {
+        var p = {
+            chart: {
+                type: 'line',
+                events: {
+                    load: function() {
+                        for (var i = 0 ; i < series.length ; i++) {
+                            var chart_series = this.series[i];
+                            plotSeries(chart_series, series[i].domain, series[i].country, series[i].item, series[i].element);
+                        }
+                    }
+                }
+            },
             title: {
-                text: 'Agricultural Total',
+                text: title,
                 x: -20
             },
             subtitle: {
@@ -48,13 +110,14 @@ var GHGEDITOR = (function() {
                 x: -20
             },
             xAxis: {
+                type: 'datetime',
                 labels: {
                     rotation: -45
                 }
             },
             yAxis: {
                 title: {
-                    text: 'CO<sub>2</sub>Eq (Gg)'
+                    text: 'CO2Eq (Gg)'
                 },
                 plotLines: [{
                     value: 0,
@@ -72,43 +135,31 @@ var GHGEDITOR = (function() {
                 borderWidth: 0
             },
             series: [{
-                name: 'FAOSTAT',
-                data: data
+                type: 'line',
+                name: series[0].name,
+                data: []
             }, {
+                type: 'line',
                 name: 'User Data',
                 data: []
             }]
-        });
+        };
+        $('#' + chart_id).highcharts(p);
     };
 
-    function addPointToChart1() {
-        var chart = $('#chart_1').highcharts();
-        var data = [];
-        var inputs = $('input[type="number"]');
-        for (var i = 0 ; i < inputs.length ; i++) {
-            var year = inputs[i].id.substring(1 + inputs[i].id.indexOf('_'));
-            var value = $(inputs[i]).val();
-            var tmp = [];
-            tmp.push(parseInt(year));
-            tmp.push(parseFloat(value));
-            data.push(tmp);
-        }
-        chart.series[1].setData(data);
-    };
-
-    function queryDB() {
+    function plotSeries(series, domain_code, country, item, element) {
         var p = {};
         p.datasource = 'faostat';
-        p.domainCode = 'GT';
+        p.domainCode = domain_code;
         p.lang = 'E';
         p.nullValues = true;
         p.thousand = '.';
         p.decimal = ',';
         p.decPlaces = 2;
         p.limit = 100;
-        p['list1Codes'] = ['\'48\''];
-        p['list2Codes'] = ['\'5058\''];
-        p['list3Codes'] = ['\'7231\''];
+        p['list1Codes'] = ['\'' + country + '\''];
+        p['list2Codes'] = ['\'' + item + '\''];
+        p['list3Codes'] = ['\'' + element + '\''];
         p['list4Codes'] = ['\'1990\'', '\'1991\'', '\'1992\'', '\'1993\'', '\'1994\'', '\'1995\'', '\'1996\'', '\'1997\'', '\'1998\'', '\'1999\'',
                            '\'2000\'', '\'2001\'', '\'2002\'', '\'2003\'', '\'2004\'', '\'2005\'', '\'2006\'', '\'2007\'', '\'2008\'', '\'2009\'',
                            '\'2010\'', '\'2011\'', '\'2012\'', '\'2013\'', '\'2014\'', '\'2015\''];
@@ -125,8 +176,16 @@ var GHGEDITOR = (function() {
                 var json = response;
                 if (typeof json == 'string')
                     json = $.parseJSON(response);
-                GHGEDITOR.CONFIG.data = json;
-                createCharts();
+                var data = [];
+                var year = 1990;
+                for (var i = json.length - 1 ; i >= 0 ; i--) {
+                    var tmp = [];
+                    tmp.push(Date.UTC(parseInt(year)));
+                    tmp.push(parseFloat(json[i][13]));
+                    data.push(tmp);
+                    year++;
+                }
+                series.setData(data);
             },
             error: function (e, b, c) {
                 console.log(e);
@@ -136,10 +195,26 @@ var GHGEDITOR = (function() {
         });
     };
 
+    function addPointToChart1() {
+        var chart = $('#chart_1').highcharts();
+        var data = [];
+        var inputs = $('input');
+        for (var i = 0 ; i < inputs.length ; i++) {
+            if (inputs[i].id.indexOf('4_') > -1) {
+                var year = inputs[i].id.substring(1 + inputs[i].id.indexOf('_'));
+                var value = $(inputs[i]).val();
+                var tmp = [];
+                tmp.push(parseInt(year));
+                tmp.push(parseFloat(value));
+                data.push(tmp);
+            }
+        }
+        chart.series[1].setData(data);
+    };
+
     return {
         CONFIG : CONFIG,
         init : init,
-        queryDB : queryDB,
         addPointToChart1:addPointToChart1
     };
 
